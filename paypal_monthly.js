c6a2acb09e6a9cc7b91ec9a7e60f56d32218d3ea
Michele Fiordispina <michele.fiordispina@countingup.com>
2022-08-03 08:58:06 +0100

    prettier

--- paypal_monthly.js
+++ paypal_monthly.js
@@ -4,17 +4,17 @@
 
     const PAYDAY = 26;
 
-    const convertAmount = (s) => Number(s.replace(/[£,]/g, ''));
+    const convertAmount = (s) => Number(s.replace(/[£,]/g, ""));
 
     const monthDiff = (d1, d2) => {
         let months = (d2.getFullYear() - d1.getFullYear()) * 12 - d1.getMonth() + d2.getMonth();
-        
+
         if (d2.getDate() >= PAYDAY) {
             months += 1;
         }
-        
+
         return months <= 0 ? 0 : months;
-    }
+    };
 
     const nextPayDay = () => {
         const today = new Date();
@@ -25,58 +25,57 @@
         }
 
         today.setDate(PAYDAY);
-        
+
         return today;
-    }
+    };
 
     const printIt = (paymentsPerMonth, total) => {
         let calculatedTotal = 0;
-        const result = paymentsPerMonth
-            .map(([monthlyRaw, minimumRaw], i) => {
-                calculatedTotal += monthlyRaw;
-                const dueDate = nextPayDay();
-                dueDate.setMonth(dueDate.getMonth() + i);
+        const result = paymentsPerMonth.map(([monthlyRaw, minimumRaw], i) => {
+            calculatedTotal += monthlyRaw;
+            const dueDate = nextPayDay();
+            dueDate.setMonth(dueDate.getMonth() + i);
 
-                const monthly = `£${monthlyRaw.toFixed(2).padStart(8, '\u2000')}`;
-                const minimum = `£${minimumRaw.toFixed(2).padStart(8, '\u2000')}`;
-                const month = `${dueDate.toLocaleString('default', { month: "long" })}:`;
+            const monthly = `£${monthlyRaw.toFixed(2).padStart(8, "\u2000")}`;
+            const minimum = `£${minimumRaw.toFixed(2).padStart(8, "\u2000")}`;
+            const month = `${dueDate.toLocaleString("default", { month: "long" })}:`;
 
-                return `${month.padEnd(10, " ")}\t${monthly}\t${minimum}`;
-            });
-        
+            return `${month.padEnd(10, " ")}\t${monthly}\t${minimum}`;
+        });
+
         if (result.length) {
             const remainder = total - calculatedTotal;
-            if (remainder > 0 && remainder !== calculatedTotal) { 
-                result.push('', `Remainder: £${remainder.toFixed(2)}`)
+            if (remainder > 0 && remainder !== calculatedTotal) {
+                result.push("", `Remainder: £${remainder.toFixed(2)}`);
             }
 
-            alert(result.join('\n'));
+            alert(result.join("\n"));
         }
-    }
+    };
 
     const doIt = () => {
         const monthlyInstalments = [0, 0, 0, 0];
         const minimumInstalments = [0, 0, 0, 0];
-        
+
         const entries = document.querySelectorAll('div[data-test-id="expand-collapse-collapsible"]');
         for (entry of entries) {
             const remainingString = entry.querySelector('p[data-test-id="remaining-balance-value"]').firstChild.nodeValue;
-            const remaining = convertAmount(remainingString)
+            const remaining = convertAmount(remainingString);
 
             const expiresString = entry.querySelector('p[data-test-id="expires-value"]').firstChild.nodeValue;
             const expiryDate = new Date(expiresString);
-            
+
             const monthsLeft = monthDiff(nextPayDay(), expiryDate);
-            
-            DEBUG && LOG(entry.querySelector('p[data-test="promoContainer_merchantName"]').firstChild.nodeValue, monthsLeft)
-            
+
+            DEBUG && LOG(entry.querySelector('p[data-test="promoContainer_merchantName"]').firstChild.nodeValue, monthsLeft);
+
             if (monthsLeft <= 0) {
                 continue;
             }
 
             const singleMonthPayment = remaining / monthsLeft;
-            
-            for (let i=0; i<monthsLeft; i++) {
+
+            for (let i = 0; i < monthsLeft; i++) {
                 monthlyInstalments[i] += singleMonthPayment;
             }
 
@@ -87,12 +86,12 @@
         const total = totalElement ? convertAmount(totalElement.textContent) : 0;
 
         const paymentsPerMonth = Array(4).fill();
-        for (let i=0; i<4; i++) {
+        for (let i = 0; i < 4; i++) {
             paymentsPerMonth[i] = [monthlyInstalments[i], minimumInstalments[i]];
         }
 
-        printIt(paymentsPerMonth, total); 
-    }
+        printIt(paymentsPerMonth, total);
+    };
 
-	doIt()
-})()
+    doIt();
+})();
