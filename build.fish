776171ef13b732b47f8dab82d826fd13d1d3dad7
Michele Fiordispina <michele.fiordispina@countingup.com>
2022-08-04 21:11:43 +0100

    build.fish argument count

--- build.fish
+++ build.fish
@@ -1,4 +1,8 @@
 #!/opt/homebrew/bin/fish
+if test (count $argv) -ne 1
+   echo "Wrong number of arguments"
+   exit 1
+end
 echo "Original size: " (stat -f%z $argv)
 set minified ( uglifyjs --compress --mangle -- $argv )
 set escaped ( string escape $minified )
