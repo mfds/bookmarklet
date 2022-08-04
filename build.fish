063aaf72c1a07b43c9c880e0f3297e7bd5097dc1
Michele Fiordispina <michele.fiordispina@countingup.com>
2022-08-04 21:08:22 +0100

    removed build_generic, made build generic

--- build.fish
+++ build.fish
@@ -1,6 +1,6 @@
 #!/opt/homebrew/bin/fish
-echo "Original size: " (stat -f%z paypal_monthly.js)
-set minified ( uglifyjs --compress --mangle -- paypal_monthly.js )
+echo "Original size: " (stat -f%z $argv)
+set minified ( uglifyjs --compress --mangle -- $argv )
 set escaped ( string escape $minified )
 set encoded ( node -p "encodeURIComponent('$escaped')" )
 set bookmarklet ( echo "javascript:(function(){;$encoded;})()" )
--- build_generic.fish
+++
@@ -1,9 +0,0 @@
-#!/opt/homebrew/bin/fish
-echo "Original size: " (stat -f%z $argv)
-set minified ( uglifyjs --compress --mangle -- $argv )
-set escaped ( string escape $minified )
-set encoded ( node -p "encodeURIComponent('$escaped')" )
-set bookmarklet ( echo "javascript:(function(){;$encoded;})()" )
-echo "Minified size: " (string length $bookmarklet)
-echo $bookmarklet | pbcopy
-echo "Copied to clipboard"
